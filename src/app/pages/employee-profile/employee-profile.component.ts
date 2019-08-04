2808304e557c0e87e0e9b1dfe9de411617aa17e8
Josué Gabuardi <jgabuardi@beenverified.com>
2019-08-04 04:36:07 -0600

    add encapsulation and animations properties in Component decorator

--- src/app/pages/employee-profile/employee-profile.component.ts
+++ src/app/pages/employee-profile/employee-profile.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {fuseAnimations} from '../../../@fuse/animations';
 
 @Component({
-  selector: 'app-employee-profile',
-  templateUrl: './employee-profile.component.html',
-  styleUrls: ['./employee-profile.component.scss']
+    selector: 'app-employee-profile',
+    templateUrl: './employee-profile.component.html',
+    styleUrls: ['./employee-profile.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations
 })
 export class EmployeeProfileComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
