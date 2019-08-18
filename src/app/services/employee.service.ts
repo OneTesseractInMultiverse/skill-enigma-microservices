b850041620315ea50d6732ed8e13f3018b7af150
Josué Gabuardi <jgabuardi@beenverified.com>
2019-08-17 18:50:31 -0600

    add getEmployeeById endpoint

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,12 +1,32 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
+
 export class EmployeeService {
 
-  constructor(private http: HttpClient) { }
+    // -------------------------------------------------------------------------------
+    // SERVICE PROPERTIES
+    // -------------------------------------------------------------------------------
+    private URL = `${environment.api}/${environment.apiVersion}/employee`;
 
+    // -------------------------------------------------------------------------------
+
+    constructor(private http: HttpClient) {
+    }
+
+    // -------------------------------------------------------------------------------
+    // GET -> RETURN A SPECIFIC EMPLOYEE DATA
+    // -------------------------------------------------------------------------------
+    public getEmployeeById(employeeId: string): Observable<Response> {
+        return this.http.get(`${this.URL}/${employeeId}`).pipe(
+            map((res: Response) => res)
+        );
+    } // GET END
 
 } // SERVICES END
