4998c246f7385f8c4bb77da62531210f41cc2d84
Pedro Guzmán <pedro.andres.guzman@ibm.com>
2019-08-20 19:35:40 -0600

    Added the capability to read the profile url from the payload comming from Employee API

--- src/app/pages/employee-profile/header/header.component.ts
+++ src/app/pages/employee-profile/header/header.component.ts
@@ -16,6 +16,7 @@ export class HeaderComponent {
     @Input() LAST_NAME: string;
     @Input() POSITION: string;
     @Input() EMAIL: string;
+    @Input() AVATAR: string;
 
     // -------------------------------------------------------------------------------
 
