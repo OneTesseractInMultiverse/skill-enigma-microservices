7f8e3a6e716c437a83fd8d14a9ae6f576df29777
Pedro A. Guzman <pedro.andres.guzman@ibm.com>
2019-10-22 14:35:45 -0600

    latest changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
     production: false,
     hmr       : false,
-    api: 'https://api.ibmcloudcr.com/skill_management',
+    // api: 'https://api.ibmcloudcr.com/skill_management',
+    api: 'https://icp-skills.appsecurity.info/skill_management',
     apiVersion: 'v1',
     authorization_server: 'https://nexo-auth.mybluemix.net/application',
     application_id: 'f20063b3-985b-470b-84e6-7794a4581b09',
